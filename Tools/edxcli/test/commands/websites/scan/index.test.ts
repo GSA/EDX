046f78b4e469a0204a7777fbb4b03d91330ff3bf
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-09-29 09:17:28 -0400

    #573 cleaning up eslint errors

--- Tools/edxcli/test/commands/websites/scan/index.test.ts
+++ Tools/edxcli/test/commands/websites/scan/index.test.ts
@@ -1,7 +1,8 @@
 import * as prompt from '@oclif/core/lib/cli-ux/prompt';
 import { expect, test } from '@oclif/test';
 
-require('dotenv').config();
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 describe('Default scan against gsa.gov', () => {
   describe('scan helper scan is called one time', () => {
